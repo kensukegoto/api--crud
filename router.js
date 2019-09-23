e60182b6d9daeaba850a943c3e98f8d40261f4da
kensuke goto <utogoken2@yahoo.co.jp>
2019-09-23 14:02:45 +0900

    [追加] idごとに記事を取得出来る

--- router.js
+++ router.js
@@ -4,8 +4,19 @@ const fs = require("fs");
 router.get("/get",(req,res)=>{
 
   const jsonObj = JSON.parse(fs.readFileSync("./data.json","utf-8"));
-
   res.json(jsonObj)
-})
+
+});
+
+router.get("/get/:id(\\d+)",(req,res)=>{
+  const id = req.params.id;
+  const jsonObj = JSON.parse(fs.readFileSync("./data.json","utf-8"));
+  const item = jsonObj.items.find(item => {
+    return +item.id === +id;
+  });
+
+  res.json((!item)?{}:item);
+
+});
 
 module.exports = router;
