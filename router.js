0aef844a38fb28cff7701195eaf9cac142a33194
kensuke goto <utogoken2@yahoo.co.jp>
2019-09-23 16:23:28 +0900

    [追加] jsonにアイテムを追加

--- router.js
+++ router.js
@@ -2,17 +2,28 @@ const router = require("express").Router();
 const fs = require("fs");
 const multer = require("multer");
 const upload = multer();
+const JSON_PATH = "./data.json"; 
+
+const tmpl = {
+  "id": "",
+  "h4": "",
+  "p" : "",
+  "time": "",
+  "ul": [],
+  "tmb": "",
+  "url": ""
+};
 
 router.get("/get",(req,res)=>{
 
-  const jsonObj = JSON.parse(fs.readFileSync("./data.json","utf-8"));
+  const jsonObj = JSON.parse(fs.readFileSync(JSON_PATH,"utf-8"));
   res.json(jsonObj)
 
 });
 
 router.get("/get/:id(\\d+)",(req,res)=>{
   const id = req.params.id;
-  const jsonObj = JSON.parse(fs.readFileSync("./data.json","utf-8"));
+  const jsonObj = JSON.parse(fs.readFileSync(JSON_PATH,"utf-8"));
   const item = jsonObj.items.find(item => {
     return +item.id === +id;
   });
@@ -23,10 +34,40 @@ router.get("/get/:id(\\d+)",(req,res)=>{
 
 router.post("/add",upload.single("tmb"),(req,res)=>{
   const {body,file} = {...req};
-  console.log(body)
-  res.json({
-    success: true
-  })
+  // h4のみ必須
+
+  const item = Object.keys(body).reduce((sum,key)=>{
+
+    switch(key){
+      case "id":
+      case "h4":
+      case "p":
+      case "time":
+      case "url":
+        sum[key] = body[key];
+        break;
+      default:
+    }
+    
+    if(/^li_\d+/.test(key)) sum.ul.push(body[key])
+    
+    return sum;
+
+  },{...tmpl})
+
+
+   /** file
+    * tml
+    */
+  
+
+  let jsonObj = JSON.parse(fs.readFileSync(JSON_PATH,"utf-8"));
+  jsonObj.items.push(item);
+  
+  fs.writeFileSync(JSON_PATH,JSON.stringify(jsonObj))
+
+  res.json(jsonObj)
+
 })
 
 module.exports = router;
